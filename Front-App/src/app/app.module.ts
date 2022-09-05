dc268ba775335bf1b9968b5ccd8ffd19f462ee9c
Alan Lozano <alan.yo.99@gmail.com>
2022-09-05 18:35:12 -0300

    se integró maqueta base

--- Front-App/src/app/app.module.ts
+++ Front-App/src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { NavBarComponent } from './componentes/nav-bar/nav-bar.component';
+import { MainMenuComponent } from './componentes/main-menu/main-menu.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { FooterComponent } from './componentes/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NavBarComponent,
+    MainMenuComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
